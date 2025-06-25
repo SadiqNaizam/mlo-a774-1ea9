9e8301d7d7021a37f2d9d9325e5adb6113274a17
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 11:52:16 +0530

    Updated code files via regeneration: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
@@ -86,4 +85,4 @@ export default {
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
